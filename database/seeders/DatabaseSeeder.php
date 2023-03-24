098dd08580fba909f18181a243e37892d1effd53
Nova Andre Saputra <novaandre.saputraadit@gmail.com>
2023-03-24 08:51:51 +0700

    ONG: adding user migration seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             ProductSeeder::class,
             BannerSeeder::class
         ]);
